92ea75dd2f6ea67ab550ee90cb0be998cb4d688f
adhik.mehta@digitalairtech.com <adhik.mehta@digitalairtech.com>
2021-10-20 10:39:57 +0530

    Error partial rendering path has been changed.

---
+++ app/overrides/user_passwords.rb
@@ -0,0 +1,19 @@
+Deface::Override.new(
+  virtual_path: 'spree/admin/user_passwords/edit',
+  name: 'Replace error partial path in edit.',
+  remove: 'erb[loud]:contains("spree/shared/error_messages")',
+  text: <<-HTML
+        <%= render :partial => 'spree/admin/shared/error_messages', :locals => { :target => @spree_user } %>
+    HTML
+    )
+
+
+Deface::Override.new(
+  virtual_path: 'spree/admin/user_passwords/new',
+  name: 'Replace error partial path in new.',
+  remove: 'erb[loud]:contains("spree/shared/error_messages")'
+  text: <<-HTML
+   in edit      <%= render :partial => 'spree/admin/shared/error_messages', :locals => { :target => @spree_user } %>
+    HTML
+    )
+
